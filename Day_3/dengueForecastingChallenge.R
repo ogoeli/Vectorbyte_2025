e2ef7a250b781999554ce8f1d77d06699e4b3fb9
Ogonna P Eli <ope4@nau.edu>
2025-06-16 07:21:37 -0700

    Day 3

--- Day_3/dengueForecastingChallenge.R
+++ Day_3/dengueForecastingChallenge.R
@@ -1,5 +1,12 @@
+## load packages
+library(tidyverse)
+library(forecast)
+library(Metrics)
+library(corrplot)
+
+
 ####### Enter Team Name ######
-teamName <- "OG"
+teamName <- "GATORS"
 
 ###### Enter path name to file containing VectorByte Training Data ######
 
@@ -26,29 +33,55 @@ dengue <- read.csv("dengueForecasting.csv")
 # with the earliest forecast (4/14/2024). Feel free to use your coding 
 # skills to avoid having to type each one out.
 
-MG <- c()
-SP <- c()
-RS <- c()
-BA <- c()
-PR <- c()
+MG <- c(10.433970, 11.122607, 11.338292, 11.271930, 11.132900, 10.856689, 10.747865,
+        10.427417,  9.881920, 9.419911,  8.899088,  8.154794) #stl, medRH, log
+SP <- c(9.867889, 10.390437, 10.683834, 10.939152, 11.097042, 11.228458,
+        11.309535, 11.368877, 11.395679, 11.426626, 11.448112, 11.455473) #Arima, minRH, log
+RS <- c(27.64109, 32.07926, 35.49469, 34.22553, 33.47458, 29.65269, 26.86080,
+        25.28273, 20.24188, 18.96964, 18.06958, 16.59861) #stl, population, sqrt
+BA <- c(31.53025, 35.62920, 38.68885, 40.35306, 40.28438, 41.81339, 44.44891,
+        45.25244, 42.13076, 41.47638, 39.08228, 33.56220) #stl, minRH, sqrt
+PR <- c(4.568701, 4.770613, 4.924596, 4.984900, 4.932586, 4.989910, 4.972878, 
+        4.837504, 4.661399, 4.534387, 4.338490, 4.116432 ) #stl, maxPressure, log
+
+compare$forecast2
+
+dengue <- read.csv("dengueForecasting.csv")
 
 
 PR <- dengue |>
   filter(uf == 'PR')
 
+BA <- dengue |>
+  filter(uf == 'BA')
+
+RS <- dengue |>
+  filter(uf == 'RS')
+
+SP <- dengue |>
+  filter(uf == 'SP')
+
+MG <- dengue |>
+  filter(uf == 'MG')
 
 
+# This code pulls them together into a data set for exporting
 
+forecasts <- data.frame(teamName = rep(teamName, 60),
+                        state = rep(c("MG", "SP", "RS", "BA",
+                                      "PR"), each = 12),
+                        epiweek = rep(seq(202416, 202427, 1), 5),
+                        cases = c(BA, MG, PR, RS, SP))
 
+####### Export Forecast .csv ######
 
-## load packages
-library(tidyverse)
-library(forecast)
-library(Metrics)
-library(corrplot)
+filename <- paste0(teamName,"_dengueChallenge_VB_2025.csv")
+
+# This will export the file to the folder where you have the data stored
+write.csv(forecasts, filename)
 
 # Read in the clean data set
-phen <- PR 
+phen <- MG
 
 ggplot(data = phen, aes(x = date, y = cases))+   
   geom_point() 
@@ -57,14 +90,14 @@ ggplot(data = phen, aes(x = date, y = population))+   #enso, iod, pdo
   geom_point() 
 
 
-phen <- PR[, !(names(PR) %in% c("test", "uf", "X", "date", "epiweek"))]
+phen <- MG[, !(names(MG) %in% c("test", "uf", "X", "date", "epiweek"))]
 
 lm1 <- lm(cases ~., data = phen)
 lm1 <- cor(phen, use = "complete.obs")
 summary(lm1)
 corrplot(lm1)
 
-phen <- PR[, !(names(PR) %in% c("test", "uf", "X", "date", "epiweek", "enso", "iod", "pdo", "minPrecip"))]
+phen <- MG[, !(names(MG) %in% c("test", "uf", "X", "date", "epiweek", "enso", "iod", "pdo", "minPrecip"))]
 
 
 phen <- log(phen)
@@ -73,9 +106,11 @@ lm1 <- cor(phen, use = "complete.obs")
 summary(lm1)
 corrplot(lm1)
 
+phen$X <- MG$X
+phen$date <- MG$date
 
-phen$X <- PR$X
-phen$date <- PR$date
+ggplot(data = phen, aes(x = date, y = cases))+   
+  geom_point() 
 
 
 ##turn our dataset into time series
@@ -92,7 +127,7 @@ acf(phen$cases, lag.max = 52)
 n <- nrow(phen)
 
 # The length of the data set minus the most recent 30 days
-trainN <- n-30
+trainN <- n-12
 testN <- trainN+1
 
 # Index the earlier rows for training
@@ -115,10 +150,9 @@ acf(fit.arima$residuals, main="")
 hist(fit.arima$residuals, main="")
 
 # make a forecast 30 days out from the train date
-arima.fc<-forecast(fit.arima, h=30)
+arima.fc<-forecast(fit.arima, h=12)
 plot(arima.fc)
 
-summary(phen)
 
 #look at the fit and predict
 plot(as.vector(phen$cases), xlab="day", ylab="pheno", type="l", col=1, lwd=2)
@@ -150,7 +184,7 @@ hist(stl.fit.arima$residuals)
 
 # We can generate forecasts with the forecast() function in the
 #    forecast package
-stl.arima.forecasts <- forecast(stl.fit.arima, h = 30)
+stl.arima.forecasts <- forecast(stl.fit.arima, h = 12)
 
 # The forecast function gives us point forecasts, as well as
 #    prediction intervals
@@ -162,7 +196,7 @@ plot(stl.arima.forecasts)
 
 ## COMPARE forecasts
 # First make a data frame with both in there
-compare <- data.frame(time = seq(1:30),
+compare <- data.frame(time = seq(1:12),
                       observed = test$cases,
                       forecast1 = arima.fc$mean,
                       forecast2 = stl.arima.forecasts$mean)
@@ -198,9 +232,9 @@ stl.fit.ets <- stlm(ts.train, s.window = "periodic", method = "ets",
 summary(stl.fit.ets$model)
 
 #Extra predictors with Linear Regression
-plot(train$maxPressure, type="l")
+plot(train$medRH, type="l")
 # We will start with just our predictors
-lm.fit <- lm(cases ~ maxPressure, 
+lm.fit <- lm(cases ~ medRH, 
              data = train)
 summary(lm.fit)
 acf(lm.fit$residuals, 52) 
@@ -209,10 +243,10 @@ acf(lm.fit$residuals, 52)
 ## 7 day moving average, not including today
 #leaves are out on trees might depend on not just the temperature today, 
 #but maybe over the last week.
-train$meanTempMA7 <- as.vector(stats::filter(ts(train$maxPressure), 
-                                             filter = c(0,rep(1/7, 7)),
+train$meanTempMA7 <- as.vector(stats::filter(ts(train$medRH), 
+                                             filter = c(0,rep(1/3, 3)),
                                              sides = 1))
-plot(train$maxPressure, type="l")
+plot(train$medRH, type="l")
 
 # Add to the training set
 train$sinSeason <- sin((2*pi*train$X)/52)
@@ -229,7 +263,7 @@ summary(temp.lm)
 acf(temp.lm$residuals)
 
 #we forecast this ahead and add it to the testing data set
-temp.fc<-forecast(temp.lm, 30)
+temp.fc<-forecast(temp.lm, 12)
 test$meanTempMA7<-temp.fc$mean
 
 # Add seasonality via sine and cosine terms to our train dataset
@@ -252,7 +286,7 @@ hist(lm.fit$residuals)
 
 # We can generate forecasts with the forecast() function in the
 #    forecast package
-lm.forecasts <- forecast(lm.fit, h = 30, newdata = test)
+lm.forecasts <- forecast(lm.fit, h = 12, newdata = test)
 
 # The forecast function gives us point forecasts, as well as
 #    prediction intervals
@@ -290,3 +324,4 @@ ggplot(data = comps.DF, aes(x = model, y = rmse))+
 ggplot(data = comps.DF, aes(x = model, y = mae))+
   geom_bar(stat = "identity", fill = "plum")+
   labs(title = "MAE Comparison")
+
